a8abd35272cc4cf00c4e776b97fcc741fac181f4
leifeng <47941608+nnlearner@users.noreply.github.com>
2022-03-09 17:00:56 +0800

    Create transformer.py

---
+++ transformer.py
@@ -0,0 +1,472 @@
+#=======================================================================================================================
+#=======================================================================================================================
+# Package Importing
+import numpy as np
+import torch
+from torch import nn
+from copy import deepcopy
+#=======================================================================================================================
+#=======================================================================================================================
+#!/usr/bin/env python3
+"""An Implement of an autoencoder with pytorch.
+This is the template code for 2020 NIAC https://naic.pcl.ac.cn/.
+The code is based on the sample code with tensorflow for 2020 NIAC and it can only run with GPUS.
+Note:
+    1.This file is used for designing the structure of encoder and decoder.
+    2.The neural network structure in this model file is CsiNet, more details about CsiNet can be found in [1].
+[1] C. Wen, W. Shih and S. Jin, "Deep Learning for Massive MIMO CSI Feedback", in IEEE Wireless Communications Letters, vol. 7, no. 5, pp. 748-751, Oct. 2018, doi: 10.1109/LWC.2018.2818160.
+    3.The output of the encoder must be the bitstream.
+"""
+import numpy as np
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+import math
+from copy import copy
+from tqdm import tqdm
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+from torch.utils.data import Dataset
+import math
+
+
+
+
+# This part implement the quantization and dequantization operations.
+# The output of the encoder must be the bitstream.
+def Num2Bit(Num, B):
+    Num_ = Num.type(torch.uint8)
+
+    def integer2bit(integer, num_bits=B * 2):
+        dtype = integer.type()
+        exponent_bits = -torch.arange(-(num_bits - 1), 1).type(dtype)
+        exponent_bits = exponent_bits.repeat(integer.shape + (1,))
+        out = integer.unsqueeze(-1) // 2 ** exponent_bits
+        return (out - (out % 1)) % 2
+
+    bit = integer2bit(Num_)
+    bit = (bit[:, :, B:]).reshape(-1, Num_.shape[1] * B)
+    return bit.type(torch.float32)
+
+
+def Bit2Num(Bit, B):
+    Bit_ = Bit.type(torch.float32)
+    Bit_ = torch.reshape(Bit_, [-1, int(Bit_.shape[1] / B), B])
+    num = torch.zeros(Bit_[:, :, 1].shape).cuda()
+    for i in range(B):
+        num = num + Bit_[:, :, i] * 2 ** (B - 1 - i)
+    return num
+
+
+class Quantization(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, B):
+        ctx.constant = B
+        step = 2 ** B
+        out = torch.round(x * step - 0.5)
+        out = Num2Bit(out, B)
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # return as many input gradients as there were arguments.
+        # Gradients of constant arguments to forward must be None.
+        # Gradient of a number is the sum of its B bits.
+        b, _ = grad_output.shape
+        grad_num = torch.sum(grad_output.reshape(b, -1, ctx.constant), dim=2) / ctx.constant
+        return grad_num, None
+
+
+class Dequantizationv1(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, B):
+        ctx.constant = B
+        step = 2 ** B
+        out = Bit2Num(x, B)
+        out = (out + 0.5) / step
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # return as many input gradients as there were arguments.
+        # Gradients of non-Tensor arguments to forward must be None.
+        # repeat the gradient of a Num for B time.
+        b, c = grad_output.shape
+        grad_output = grad_output.unsqueeze(2) / ctx.constant
+        grad_bit = grad_output.expand(b, c, ctx.constant)
+        return torch.reshape(grad_bit, (-1, c * ctx.constant)), None
+
+class Dequantization(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, B):
+        ctx.constant = B
+        step = 2 ** B
+        out = Bit2Num(x, B)
+        out = (out + 0.5) / step
+        return out
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        # return as many input gradients as there were arguments.
+        # Gradients of non-Tensor arguments to forward must be None.
+        # repeat the gradient of a Num for four time.
+        #b, c = grad_output.shape
+        #grad_bit = grad_output.repeat(1, 1, ctx.constant)
+        #return torch.reshape(grad_bit, (-1, c * ctx.constant)), None
+        grad_bit = grad_output.repeat_interleave(ctx.constant, dim=1)
+        return grad_bit, None
+
+class QuantizationLayer(nn.Module):
+
+    def __init__(self, B):
+        super(QuantizationLayer, self).__init__()
+        self.B = B
+
+    def forward(self, x):
+        out = Quantization.apply(x, self.B)
+        return out
+
+
+class DequantizationLayer(nn.Module):
+
+    def __init__(self, B):
+        super(DequantizationLayer, self).__init__()
+        self.B = B
+
+    def forward(self, x):
+        out = Dequantization.apply(x, self.B)
+        return out
+
+    
+class Swish(nn.Module):
+    def __init__(self, inplace=False):
+        super().__init__()
+        self.inplace = inplace
+
+    def forward(self, x):
+        if self.inplace:
+            x.mul_(torch.sigmoid(x))
+            return x
+        else:
+            return x * torch.sigmoid(x)
+
+from collections import OrderedDict
+class ActBNConv(nn.Sequential):
+    def __init__(self, in_planes, out_planes, kernel_size, stride=1, groups=1):
+        if not isinstance(kernel_size, int):
+            padding = [(i - 1) // 2 for i in kernel_size]
+        else:
+            padding = (kernel_size - 1) // 2
+        super(ActBNConv, self).__init__(OrderedDict([
+            ('act', Swish()),
+            ('bn', nn.BatchNorm2d(in_planes)),
+            ('conv', nn.Conv2d(in_planes, out_planes, kernel_size, stride,
+                               padding=padding, groups=groups, bias=False))
+        ]))
+
+
+def sub_pix(in_planes, out_planes, upscale_factor):
+    mid_planes = upscale_factor**2 * out_planes
+    return nn.Sequential(
+        nn.Conv2d(in_planes, mid_planes, kernel_size=1, stride=1, bias=False),
+        nn.BatchNorm2d(mid_planes),
+        nn.PixelShuffle(upscale_factor)
+    )
+    
+''' Define the Transformer model '''
+
+class ScaledDotProductAttention(nn.Module):
+    ''' Scaled Dot-Product Attention '''
+
+    def __init__(self, temperature, attn_dropout=0.1):
+        super().__init__()
+        self.temperature = temperature
+        self.dropout = nn.Dropout(attn_dropout)
+
+    def forward(self, q, k, v, mask=None):
+
+        attn = torch.matmul(q / self.temperature, k.transpose(2, 3))
+
+        if mask is not None:
+            attn = attn.masked_fill(mask == 0, -1e9)
+
+        attn = self.dropout(F.softmax(attn, dim=-1))
+        output = torch.matmul(attn, v)
+
+        return output, attn
+
+
+class MultiHeadAttention(nn.Module):
+    ''' Multi-Head Attention module '''
+
+    def __init__(self, n_head, d_model, d_k, d_v, dropout=0.1):
+        super().__init__()
+
+        self.n_head = n_head
+        self.d_k = d_k
+        self.d_v = d_v
+
+        self.w_qs = nn.Linear(d_model, n_head * d_k, bias=False)
+        self.w_ks = nn.Linear(d_model, n_head * d_k, bias=False)
+        self.w_vs = nn.Linear(d_model, n_head * d_v, bias=False)
+        self.fc = nn.Linear(n_head * d_v, d_model, bias=False)
+
+        self.attention = ScaledDotProductAttention(temperature=d_k ** 0.5)
+
+        self.dropout = nn.Dropout(dropout)
+        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+
+
+    def forward(self, q, k, v, mask=None):
+
+        d_k, d_v, n_head = self.d_k, self.d_v, self.n_head
+        sz_b, len_q, len_k, len_v = q.size(0), q.size(1), k.size(1), v.size(1)
+
+        residual = q
+
+        # Pass through the pre-attention projection: b x lq x (n*dv)
+        # Separate different heads: b x lq x n x dv
+        q = self.w_qs(q).view(sz_b, len_q, n_head, d_k)
+        k = self.w_ks(k).view(sz_b, len_k, n_head, d_k)
+        v = self.w_vs(v).view(sz_b, len_v, n_head, d_v)
+
+        # Transpose for attention dot product: b x n x lq x dv
+        q, k, v = q.transpose(1, 2), k.transpose(1, 2), v.transpose(1, 2)
+
+        if mask is not None:
+            mask = mask.unsqueeze(1)   # For head axis broadcasting.
+
+        q, attn = self.attention(q, k, v, mask=mask)
+
+        # Transpose to move the head dimension back: b x lq x n x dv
+        # Combine the last two dimensions to concatenate all the heads together: b x lq x (n*dv)
+        q = q.transpose(1, 2).contiguous().view(sz_b, len_q, -1)
+        q = self.dropout(self.fc(q))
+        q += residual
+
+        q = self.layer_norm(q)
+
+        return q, attn
+
+
+class PositionwiseFeedForward(nn.Module):
+    ''' A two-feed-forward-layer module '''
+
+    def __init__(self, d_in, d_hid, dropout=0.1):
+        super().__init__()
+        self.w_1 = nn.Linear(d_in, d_hid) # position-wise
+        self.w_2 = nn.Linear(d_hid, d_in) # position-wise
+        self.layer_norm = nn.LayerNorm(d_in, eps=1e-6)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+
+        residual = x
+
+        x = self.w_2(F.relu(self.w_1(x)))
+        x = self.dropout(x)
+        x += residual
+
+        x = self.layer_norm(x)
+
+        return x
+
+class EncoderLayer(nn.Module):
+    ''' Compose with two layers '''
+
+    def __init__(self, d_model, d_inner, n_head, d_k, d_v, dropout=0.1):
+        super(EncoderLayer, self).__init__()
+        self.slf_attn = MultiHeadAttention(n_head, d_model, d_k, d_v, dropout=dropout)
+        self.pos_ffn = PositionwiseFeedForward(d_model, d_inner, dropout=dropout)
+
+    def forward(self, enc_input, slf_attn_mask=None):
+        enc_output, enc_slf_attn = self.slf_attn(enc_input, enc_input, enc_input, mask=slf_attn_mask)
+        enc_output = self.pos_ffn(enc_output)
+        return enc_output, enc_slf_attn
+
+
+class DecoderLayer(nn.Module):
+    ''' Compose with three layers '''
+
+    def __init__(self, d_model, d_inner, n_head, d_k, d_v, dropout=0.1):
+        super(DecoderLayer, self).__init__()
+        self.slf_attn = MultiHeadAttention(n_head, d_model, d_k, d_v, dropout=dropout)
+        self.enc_attn = MultiHeadAttention(n_head, d_model, d_k, d_v, dropout=dropout)
+        self.pos_ffn = PositionwiseFeedForward(d_model, d_inner, dropout=dropout)
+
+    def forward(self, dec_input, enc_output, slf_attn_mask=None, dec_enc_attn_mask=None):
+        dec_output, dec_slf_attn = self.slf_attn(dec_input, dec_input, dec_input, mask=slf_attn_mask)
+        dec_output, dec_enc_attn = self.enc_attn(dec_output, enc_output, enc_output, mask=dec_enc_attn_mask)
+        dec_output = self.pos_ffn(dec_output)
+        return dec_output, dec_slf_attn, dec_enc_attn
+    
+
+def get_pad_mask(seq, pad_idx):
+    return (seq != pad_idx).unsqueeze(-2)
+
+def all_one_mask(img):
+    ones = torch.ones_like(img)
+    return ones>0
+
+
+def get_subsequent_mask(seq):
+    ''' For masking out the subsequent info. '''
+    sz_b, len_s = seq.size()
+    subsequent_mask = (1 - torch.triu(
+        torch.ones((1, len_s, len_s), device=seq.device), diagonal=1)).bool()
+    return subsequent_mask
+
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_hid, n_position=200):
+        super(PositionalEncoding, self).__init__()
+
+        # Not a parameter
+        self.register_buffer('pos_table', self._get_sinusoid_encoding_table(n_position, d_hid))
+
+    def _get_sinusoid_encoding_table(self, n_position, d_hid):
+        ''' Sinusoid position encoding table '''
+        # TODO: make it with torch instead of numpy
+
+        def get_position_angle_vec(position):
+            return [position / np.power(10000, 2 * (hid_j // 2) / d_hid) for hid_j in range(d_hid)]
+
+        sinusoid_table = np.array([get_position_angle_vec(pos_i) for pos_i in range(n_position)])
+        sinusoid_table[:, 0::2] = np.sin(sinusoid_table[:, 0::2])  # dim 2i
+        sinusoid_table[:, 1::2] = np.cos(sinusoid_table[:, 1::2])  # dim 2i+1
+        return torch.FloatTensor(sinusoid_table).unsqueeze(0)
+
+    def forward(self, x):
+        return x + self.pos_table[:, :x.size(1)].clone().detach()
+
+
+class Encoder(nn.Module):
+    ''' A encoder model with self attention mechanism. '''
+
+    def __init__(
+            self, d_word_vec, n_layers, n_head, d_k, d_v,
+            d_model, d_inner, dropout=0.1, n_position=200):
+
+        super().__init__()
+
+        self.position_enc = PositionalEncoding(d_word_vec, n_position=n_position)
+        self.dropout = nn.Dropout(p=dropout)
+        self.layer_stack = nn.ModuleList([
+            EncoderLayer(d_model, d_inner, n_head, d_k, d_v, dropout=dropout)
+            for _ in range(n_layers)])
+        self.layer_norm = nn.LayerNorm(d_model, eps=1e-6)
+        self.d_model = d_model
+
+    def forward(self, enc_output, src_mask, return_attns=False):
+
+        enc_slf_attn_list = []
+
+        # -- Forward
+        enc_output = self.dropout(self.position_enc(enc_output))
+        # enc_output = self.position_enc(enc_output)
+        enc_output = self.layer_norm(enc_output)
+
+        for enc_layer in self.layer_stack:
+            enc_output, enc_slf_attn = enc_layer(enc_output, slf_attn_mask=src_mask)
+            enc_slf_attn_list += [enc_slf_attn] if return_attns else []
+
+        if return_attns:
+            return enc_output, enc_slf_attn_list
+        return enc_output,
+
+
+
+class CSIEncoder(nn.Module):
+    
+    def __init__(
+            self, feedback_bits=512, 
+            d_word_vec=32, d_model=32, d_inner=2048,
+            n_layers=2, n_head=8, d_k=32, d_v=32, dropout=0.1, n_position=128*2, B=2):
+        super(CSIEncoder,self).__init__()
+        print('encoder feedback_bits: ', feedback_bits)
+        self.feedback_bits = feedback_bits
+        self.B = B
+
+        self.encoder = Encoder(
+            n_position=n_position,
+            d_word_vec=d_word_vec, d_model=d_model, d_inner=d_inner,
+            n_layers=n_layers, n_head=n_head, d_k=d_k, d_v=d_v,
+            dropout=dropout)
+        
+
+        self.fc = nn.Linear(1*32*256, int(self.feedback_bits//self.B))
+        self.out_bn = nn.BatchNorm1d(int(self.feedback_bits // self.B))
+
+        self.sig = nn.Sigmoid()
+        self.quantize = QuantizationLayer(self.B)
+        
+    def forward(self, csi_image):
+        csi_image = csi_image.permute((0,3,1,2,4)).reshape(-1, 32, 256).permute((0,2,1))
+
+        enc_output, *_ = self.encoder(csi_image, None)
+        out = self.out_bn(self.fc(enc_output.view(-1,32*256)))
+        out = self.sig(out)
+        
+        
+        
+        
+        out = self.quantize(out)
+        
+        return out
+
+
+class CSIDecoder(nn.Module):
+    
+    def __init__(
+            self, feedback_bits=512,
+            d_word_vec=32, d_model=32, d_inner=2048,
+            n_layers=2, n_head=8, d_k=32, d_v=32, dropout=0.1, n_position=256, B=2):
+        super(CSIDecoder,self).__init__()
+        print('decoder feedback_bits: ', feedback_bits)
+        self.feedback_bits = feedback_bits
+
+        self.B = B
+        
+        self.decoder = Encoder(
+            n_position=n_position,
+            d_word_vec=d_word_vec, d_model=d_model, d_inner=d_inner,
+            n_layers=n_layers, n_head=n_head, d_k=d_k, d_v=d_v,
+            dropout=dropout)
+
+        
+        self.fc = nn.Linear(int(self.feedback_bits//self.B), 1*32*256)
+        
+        self.sig = nn.Sigmoid()
+        self.dequantize = DequantizationLayer(self.B)
+        
+    def forward(self, bits):
+        bits = self.dequantize(bits)
+        bits = torch.log(bits / (1.0 - bits))
+        
+        # print(bits.size())
+
+        bits = torch.cat([bits], dim=1)
+
+        csi_image = self.fc(bits).reshape(-1,256,32)
+        dec_output, *_ = self.decoder(csi_image, None)
+        out = dec_output.permute((0,2,1)).reshape((-1,32,4,32,2)).permute((0,2,3,1,4))
+        out = self.sig(out)-0.5
+
+        return out
+
+
+class AutoEncoder(nn.Module):
+
+    def __init__(self, feedback_bits, dropout=0.1, B=2):
+        super(AutoEncoder, self).__init__()
+        self.encoder = CSIEncoder(feedback_bits, B=B, dropout=dropout)
+        self.decoder = CSIDecoder(feedback_bits, B=B, dropout=dropout)
+
+    def forward(self, x):
+        # x: bx4x32x32x2
+        feature = self.encoder(x)
+        out = self.decoder(feature)
+        return out
